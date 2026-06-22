08d20cc318ea2519e7c985e0c90eb83f0b82459e
ab25cq <ab25cq@gmail.com>
2026-06-22 17:06:16 +0900

    iii

--- update.sh
+++ update.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
 export EDITOR=vin
-git config --global user.email ab25cq@icloud.com
+git config --global user.email ab25cq@gmail.com
 git config --global user.name ab25cq
-
-sh remove_all_build.sh
 
 git add .
 git commit 
 git remote add origin git@github.com:ab25cq/c-.git
 git remote set-url origin git@github.com:ab25cq/c-.git
 git push --force origin main 
-
 
