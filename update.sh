978955eed6bcd5bd5f7ffdb479c1875f849fadd3
ab25cq <ab25cq@gmail.com>
2026-06-22 17:08:25 +0900

    iii

--- update.sh
+++ update.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
+set -eu
 
 export EDITOR=vin
 git config --global user.email ab25cq@gmail.com
 git config --global user.name ab25cq
 
 git add .
-git commit 
-git remote add origin git@github.com:ab25cq/c-.git
-git remote set-url origin git@github.com:ab25cq/c-.git
-git push --force origin main 
+if ! git diff --cached --quiet; then
+    git commit
+fi
 
+if ! git remote get-url origin >/dev/null 2>&1; then
+    git remote add origin git@github.com:ab25cq/c-.git
+fi
+git remote set-url origin git@github.com:ab25cq/c-.git
+git push origin HEAD:main
